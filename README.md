560132ea888440482fc3977e1b9fa899edc33441
Diego Alonso VP <diegovilcapena@gmail.com>
2026-06-05 09:20:04 -0500

    Primer commit

---
+++ README.md
@@ -0,0 +1,9 @@
+#WebService que sirve de datos a una aplicación Android Java
+
+## Procedimientos de ejecución
+
+1. Clonar repositorio
+2. Ejecutar npm install
+3. Restaurar la db
+4. Configurar variables de entorno
+5. Ejecutar la aplicación node server
---
+++ database/db.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE constructora;
+USE constructora;
+
+CREATE TABLE herramientas
+(
+  idherramienta  INT AUTO_INCREMENT PRIMARY KEY,
+  nombre         VARCHAR(50)   NOT NULL,
+  marca          VARCHAR(50)   NOT NULL,
+  descripcion    VARCHAR(100)  NOT NULL,
+  fotografia     VARCHAR(200)  NULL,
+  condicion      ENUM('Bueno','Regular','Malo')  NOT NULL DEFAULT 'Bueno',
+  tipo           ENUM('Herramienta', 'Instrumento')
+)ENGINE = INNODB;
+
+INSERT INTO herramientas
+  (nombre, marca, descripcion, tipo)  VALUES
+  ('Alicates', 'Kamasa', 'Para trabajos eléctricos', 'Manual'),
+  ('Destornillador', 'PB', 'Tipo cruz imantado', 'Manual');
+
+INSERT INTO herramientas
+  (nombre, marca, descripcion) VALUES
+  ('Amoladora', 'Bosch', '500 Watts'),
+  ('Taladro', 'DeWalt', 'Inalámbrico 18V');
+
+  SELECT * FROM herramientas;
